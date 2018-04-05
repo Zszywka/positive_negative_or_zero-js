eff00defcd80566e4d38c731cecda98480a907f7
Joanna <piotrowskaWIMIIP@gmail.com>
2018-04-05 14:37:15 +0200

    Change of file support JS under the HTML file

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,24 @@ function myFunction() {
   var b = prompt('write value b');
   var value =(a*a)-(2*a*b)-(b*b);
 
-  if ((a != null) && (b != null) && (value>0)) {
-    document.getElementById("value").innerHTML='This phrase (a^2)-2ab-(b^2) is equal to: '+ value + '. The result is positive';
-    console.log('This value:(a^2)-2ab-(b^2) is is equal to: '+ value + '. The result and is positive.');
-
-  } else if((a != null) && (b != null) &&(value<0)) {
-    document.getElementById("value").innerHTML='This phrase (a^2)-2ab-(b^2) is equal to: '+ value + '. The result is negative';
-    console.log('This value:(a^2)-2ab-(b^2) is is equal to: '+ value + '. The result is negative');
-  } else {
-      document.getElementById("value").innerHTML='This phrase (a^2)-2ab-(b^2) is equal zero: ' + value;
-    console.log('This value:(a^2)-2ab-(b^2) is is equal to: '+ value + '. The result is zero');
+  if ((a != null) && (b != null)) {
+    if(value>0) {
+      document.getElementById("demo").innerHTML='This phrase (a^2)-2ab-(b^2) is equal to: '+ value + '. The result is positive';
+      console.log('This value:(a^2)-2ab-(b^2) is is equal to: '+ value + '. The result and is positive.');
+    }
+    else if(value<0) {
+      document.getElementById("demo").innerHTML='This phrase (a^2)-2ab-(b^2) is equal to: '+ value + '. The result is negative';
+      console.log('This value:(a^2)-2ab-(b^2) is is equal to: '+ value + '. The result is negative');
+    }
+    else {
+      document.getElementById("demo").innerHTML='This phrase (a^2)-2ab-(b^2) is equal zero: ' + value;
+      console.log('This value:(a^2)-2ab-(b^2) is is equal to: '+ value + '. The result is zero');
+    }
+  }
+  else {
+    document.getElementById("demo").innerHTML='no valid entered value b or/and a';
+    console.log('no valid entered value b or/and a');
   }
 }
+
+document.getElementById("value").addEventListener("click",myFunction);
